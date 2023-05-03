591f7ef2700e873613ddacaa1d2acaaeda132503
Luiz F. Picolo <luizpicolo@gmail.com>
2023-05-03 14:27:58 -0400

    Commit HTML

--- script.js
+++ script.js
@@ -9,7 +9,14 @@ class Produto {
     }
 
     mostrar_dados(){
-        return this.nome +  this.datadecadastro + this.descricao + this.preco;
+        return `
+        <div div>${this.nome}</div>
+        <div div>${this.datadecadastro}</div>
+        <div div>${this.descricao}</div>
+        <div div>${this.preco}</div>
+        `
+       // return this.nome +  this.datadecadastro + this.descricao + this.preco;//
+
     }
 }
 
@@ -17,12 +24,24 @@ class ProdutoDestaque extends Produto{
         constructor(nome, datadecadastro , inscricao, preco, produto_destaque){
             super(nome, datadecadastro, inscricao, preco );
             this.produto_destaque = produto_destaque;
+            
         }
         mostrarProdutoDestaque(){
-            return this.nome + this.datadecadastro + this.descricao + this.preco + this.produto_destaque
+            return `
+            <div div>${this.nome}</div>
+            <div div>${this.datadecadastro}</div>
+            <div div>${this.descricao}</div>
+            <div div>${this.preco}</div>
+            <div div>${this.produto_destaque}</div>
+            `
+
+            //return this.nome + this.datadecadastro + this.descricao + this.preco + this.produto_destaque//
         }
 }
-    
-    
+
+const produtom = ProdutoDestaque("garrafa Harry Potter ", "14/10/2022", "garrafa Harry Poter", "100,30", "https://mimeria.fbitsstatic.net/img/p/garrafa-termica-buck-harry-potter-mapa-do-marato-400-ml-76719/263395-1.jpg?w=770&h=770&v=no-change&qs=ignore");
+console.log(mostrarProdutosDestaque());    
+document.getElementById("produtoDestaque");
+div.inersetjacentHTML('afterbegin',mostrarProdutosDestaque())  ;
 
 
